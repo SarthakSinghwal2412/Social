3a8bd4a53a786ca757cdebb64485a5e57cad6f3e
SarthakSinghwal2412 <sarthaksinghwal2412@gmail.com>
2022-06-11 15:09:23 +0530

    before follow and unfollow user

--- api/index.js
+++ api/index.js
@@ -7,7 +7,9 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
 
+const path = require("path")
 dotenv.config();
 
 mongoose.connect(
@@ -17,11 +19,31 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
-
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
